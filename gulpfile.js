c6cef7e8c20f3ce7a56ebcd1d16d8ff38036c338
Derek Peterson <derek.peterson@nbcuni.com>
2016-07-12 10:08:47 -0700

    Have a Freewheel ad run once

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var exec = require('child_process').exec;
+
+gulp.task('img:copy', function () {
+  return gulp.src('./lib/img/*')
+    .pipe(rename({
+      dirname: ''
+    }))
+    .pipe(gulp.dest('./static/img'));
+});
 
 gulp.task('js:compile', function () {
   return browserify({
@@ -16,17 +25,21 @@ gulp.task('js:compile', function () {
       ]
     })
     .bundle()
+    .on('error', function (err) {
+      gutil.log(err);
+      this.emit('end');
+    })
     .pipe(source('./lib/js/main.js'))
     .pipe(buffer())
     .pipe(rename('main.js'))
     .pipe(gulp.dest('./static/js'));
 });
 
-gulp.task('dev', ['js:compile'], function () {
+gulp.task('dev', ['img:copy', 'js:compile'], function () {
   gulp.watch('./lib/js/**/*.js', ['js:compile']);
   exec('npm run serve', function (err, stdout, stderr) {
-    if (error) {
-      console.error(`exec error: ${error}`);
+    if (err) {
+      console.error(`exec error: ${err}`);
       return;
     }
     console.log(`stdout: ${stdout}`);
@@ -34,4 +47,4 @@ gulp.task('dev', ['js:compile'], function () {
   });
 });
 
-gulp.task('default', ['js:compile']);
+gulp.task('default', ['img:copy', 'js:compile']);
