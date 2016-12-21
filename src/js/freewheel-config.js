a1ad9e6aac85645c333c174af1c68f5e359f9571
StuWilli <stu.r.williams@gmail.com>
2016-12-21 15:49:08 +0000

    updated network

--- src/js/freewheel-config.js
+++ src/js/freewheel-config.js
@@ -1,26 +1,20 @@
 const defaultConfig = {
   networkId: 169843,
-  serverUrl: 'http://29773.v.fwmrm.net/ad/p/1',
-  siteSectionCustomId: 'today_canonical_video',
+  serverUrl: 'http://7b94f.v.fwmrm.net/ad/p/1',
+  siteSectionCustomId: 'TEST_SSID',
   siteSectionFallbackId: '884196',
-  temporalSlots: [{
-    adUnit: 'preroll',
-    id: 'Preroll_1',
-    timePosition: 0
-  }],
-  videoAssetCustomId: 'tdy_sterns_amazon_160712',
-  videoAssetFallbackId: '90320017',
-  videoAssetDuration: 235
+  videoAssetCustomId: 'STU_TEST_ASSET_1',
+  videoAssetFallbackId: '111560574'
 };
 
 module.exports = {
   Flash: Object.assign({}, defaultConfig, {
-    profile: 'nbcnews_flash_live',
+    profile: '506191:ATN_AS3_Video_Test',
     sdkurl: 'http://adm.fwmrm.net/p/release/latest-AS3/adm/dbg/AdManager.swf?logLevel=VERBOSE',
     swfurl: '//players.brightcove.net/videojs-freewheel/2/videojs.freewheel.swf'
   }),
   Html5: Object.assign({}, defaultConfig, {
-    profile: 'nbcnews_html5_live',
+    profile: '506191:ATN_HTML5_Test',
     sdkurl: 'http://adm.fwmrm.net/p/release/latest-JS/adm/prd/AdManager.js?logLevel=VERBOSE'
   }),
   adTechOrder: [
